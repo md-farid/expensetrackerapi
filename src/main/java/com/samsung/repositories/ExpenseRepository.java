b9b78d177596f27a5d206394f14c338eb8857859
md.farid <md.farid.ice@gmail.com>
2023-01-08 00:48:36 +0600

    repository annotation added

--- src/main/java/com/samsung/repositories/ExpenseRepository.java
+++ src/main/java/com/samsung/repositories/ExpenseRepository.java
@@ -2,6 +2,8 @@ package com.samsung.repositories;
 
 import com.samsung.entities.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExpenseRepository extends JpaRepository<Expense,Long> {
 }
