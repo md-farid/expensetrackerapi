ae84130920994316a7fadae313ba14046da9245e
md.farid <md.farid.ice@gmail.com>
2023-01-10 23:11:22 +0600

    create password encoder

--- src/main/java/com/samsung/config/ProjectSecurityConfiguration.java
+++ src/main/java/com/samsung/config/ProjectSecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -52,5 +54,10 @@ public class ProjectSecurityConfiguration {
         inMemoryUserDetailsManager.createUser(user);
         inMemoryUserDetailsManager.createUser(admin);
         return inMemoryUserDetailsManager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
