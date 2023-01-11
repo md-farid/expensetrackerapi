4f2f850c11bb16b10db011fe62573771899f63c0
md.farid <md.farid.ice@gmail.com>
2023-01-11 22:09:54 +0600

    unused import remove

--- src/main/java/com/samsung/config/ProjectSecurityConfiguration.java
+++ src/main/java/com/samsung/config/ProjectSecurityConfiguration.java
@@ -5,10 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
