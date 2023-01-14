65f4b99701ec6733e2f581919ffdd110e2102d4f
md.farid <md.farid.ice@gmail.com>
2023-01-14 23:03:29 +0600

    refactor the code of login api

--- src/main/java/com/samsung/controllers/AuthController.java
+++ src/main/java/com/samsung/controllers/AuthController.java
@@ -1,18 +1,21 @@
 package com.samsung.controllers;
 
 import com.samsung.entities.User;
+import com.samsung.models.JWTResponse;
 import com.samsung.models.LoginModel;
 import com.samsung.models.UserModel;
+import com.samsung.security.CustomUserDetailsService;
 import com.samsung.services.UserService;
+import com.samsung.utils.JwtTokenUtil;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +27,13 @@ public class AuthController {
     private UserService userService;
 
     @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
     @PostMapping("/register")
     public ResponseEntity<User> createUser(@Valid @RequestBody UserModel userModel){
@@ -32,12 +41,26 @@ public class AuthController {
     }
 
     @PostMapping("/auth")
-    public ResponseEntity<HttpStatus> login(@RequestBody LoginModel loginModel){
+    public ResponseEntity<JWTResponse> login(@RequestBody LoginModel loginModel) throws Exception {
 
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-             loginModel.getEmail(),loginModel.getPassword()
-        ));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+        authenticate(loginModel.getEmail(),loginModel.getPassword());
+
+        // we need to generate jwt token
+        final UserDetails userDetails = customUserDetailsService.loadUserByUsername(loginModel.getEmail());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return new ResponseEntity<JWTResponse>(new JWTResponse(token),HttpStatus.OK);
+    }
+
+    private void authenticate(String email, String password) throws Exception {
+
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email,password));
+        }catch (DisabledException ex){
+            throw new Exception("User disabled.");
+        }catch (BadCredentialsException ex){
+            throw new Exception("Bad credentials.");
+        }
+
     }
 }
---
+++ src/main/java/com/samsung/utils/JwtTokenUtil.java
@@ -0,0 +1,11 @@
+package com.samsung.utils;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenUtil {
+    public String generateToken(UserDetails userDetails){
+        return null;
+    }
+}
