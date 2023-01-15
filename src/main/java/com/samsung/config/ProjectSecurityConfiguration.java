713b991c85457f1d94af4e7fd95a3c7ee4b97b3e
md.farid <md.farid.ice@gmail.com>
2023-01-15 23:24:01 +0600

    project done

--- src/main/java/com/samsung/config/ProjectSecurityConfiguration.java
+++ src/main/java/com/samsung/config/ProjectSecurityConfiguration.java
@@ -1,16 +1,25 @@
 package com.samsung.config;
 
+import com.samsung.security.JwtRequestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class ProjectSecurityConfiguration {
+
+    @Bean
+    JwtRequestFilter jwtRequestFilter(){
+        return new JwtRequestFilter();
+    }
 
     @Bean
     SecurityFilterChain defSecurityFilterChain(HttpSecurity http) throws Exception{
@@ -20,8 +29,10 @@ public class ProjectSecurityConfiguration {
                 .authorizeHttpRequests()
                 .requestMatchers("/auth","/register").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin()
-                .and().httpBasic();
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.formLogin();
+        http.httpBasic();
         return http.build();
     }
 
